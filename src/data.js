1ca01b952d2297ff6f4e0ed7eaed2459f98069f1
lqasemzadeh <lqasemzadeh@gmail.com>
2024-03-09 16:29:46 +0330

    React-Table

--- src/data.js
+++ src/data.js
@@ -6,9 +6,7 @@ import { faker } from '@faker-js/faker';
 export function createRandomUser() {
     return {
         Token: faker.image.avatar(),
-        Balance: faker.datatype.number(1000),
-        Deposit: faker.datatype.number(1000),
-        Withdraw: faker.datatype.number(1000),
+        Price: faker.datatype.number(1000),
     };
 }
 
--- src/pages/PriceTable.page.jsx
+++ src/pages/PriceTable.page.jsx
@@ -10,10 +10,22 @@ const PriceTablePage = () => {
 
 
     const columns = [
+        columnHelper.accessor("", {
+           id: "#",
+           cell: (info) => <span>{info.row.index + 1}</span>,
+           header: "#",
+        }),
         columnHelper.accessor("Token",{
-            cell: (info)=> <span>{info.getValue()}</span>
+            cell: (info)=> (
+                <img src={info?.getValue()} alt="" className="rounded-full w-10 h-10 object-cover" />
+            ),
+            header: "Token",
+        }),
+        columnHelper.accessor("Price",{
+            cell: (info)=> <span>{info.getValue()}</span>,
+            header: "Price",
+        }),
 
-        })
     ]
 
     const [data] = useState(()=>[...USERS])
@@ -29,25 +41,34 @@ const PriceTablePage = () => {
     return (
 
         <div className="p-2 max-w-5xl mx-auto text-white fill-gray-400">
-            <table>
-                {
-                    table.getHeaderGroups().map((headerGroup)=>(
-                        <tr key={headerGroup.id}>
-                            {
-                                headerGroup.headers.map(header=>(
-                                    <th key={header.id}>
-                                        {
-                                            flexRender(header.column.columnDef.header, header.getContext())
-                                        }
-                                    </th>
-                                ))
-                            }
+            <table className="border border-gray-700 w-full text-left">
+                <thead className="bg-indigo-600">
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => (
+                            <th key={header.id} className="capitalize px-3.5 py-3">
+                                {flexRender(header.column.columnDef.header, header.getContext())}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </thead>
+
+                <tbody>
+                {table.getRowModel().rows.length ? (
+                    table.getRowModel().rows.map((row) => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
                         </tr>
-
                     ))
-
-                }
+                ) : null}
+                </tbody>
             </table>
+
 
         </div>
     );
